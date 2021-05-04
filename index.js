601197c61ba4e09d8035f421f5b886e1e9dc387c
repicco <renato.picco@newpost.com.br>
2021-05-04 11:40:22 -0300

    jogo da velha 1.0 finalizado

--- index.js
+++ index.js
@@ -1,46 +1,68 @@
+const buttonPlay = document.querySelector('[data-jogo="inicia"]')
+const buttonReset = document.querySelector('[data-jogo="recomeca"]')
 
-const areaDoGame = document.querySelector('.area-game')
-const jogadas = ['X', 'O']
-let player = 0
-
-let jogadasComVitoria = [
-    [0, 1, 2], [3, 4, 5], [6, 7, 8],
-    [0, 3 ,6], [1, 4, 7], [2, 5, 8],
-    [0, 4, 8], [2, 4, 6]
-]
-
-function fazAJogadaNoClique({currentTarget}) {
-    currentTarget.classList.add('indisponivel')
-    if (player === 1) {
-        currentTarget.innerHTML = jogadas[player]
-        currentTarget.dataset.jogada="O"
-        verificarSeGanhou();
-        player = 0
-    } else {
-        currentTarget.innerHTML = jogadas[player]
-        currentTarget.dataset.jogada="X"
-        verificarSeGanhou();
-        player = 1
-    }
-    camposJogaveis();
-}
-
-function verificarSeGanhou() {
-    let campos = [];
-    let jogadasValidas = [];
-    const campoPreenchido = [...areaDoGame.children].forEach((item, index)=> {
-        if (item.innerHTML === jogadas[player]) {
-            campos.push(index);
+const jogo_da_velha = {
+    areaDoGame: null,
+    fields_of_game: ['', '', '', '', '', '', '', '', ''], 
+    player_simbol: {
+        options: ['X', 'O'],
+        player: 0,
+        changePlayer: function() {
+            this.player = this.player === 0 ? 1 : 0;
         }
-    })
+    },
+    gameover: false,
+    plays_with_victory: [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3 ,6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ],
+
+    init(container) {
+        this.areaDoGame = document.querySelector(container)
+        this.draw();
+    }, 
+
+    make_play(position) {
+        if (this.gameover) return false;
+        if (this.fields_of_game[position] === '') {
+            this.fields_of_game[position] = this.player_simbol.options[this.player_simbol.player]
+            this.draw();
+            this.check_victory(this.player_simbol.options[this.player_simbol.player])
+            this.player_simbol.changePlayer();
+        }
+    },
+
+    check_victory(simbol) {
+        for (i in this.plays_with_victory) {    
+            if( this.fields_of_game[ this.plays_with_victory[i][0] ] === simbol &&
+                this.fields_of_game[ this.plays_with_victory[i][1] ] === simbol &&
+                this.fields_of_game[ this.plays_with_victory[i][2] ] === simbol) {
+                    this.gameover = true;
+                    
+                    for ( field in this.plays_with_victory[i] ) {
+                        this.areaDoGame.children[this.plays_with_victory[i][field]].classList.add('winner')
+                    }
+            }
+        }
+    },
+
+    reset_game() {
+        this.areaDoGame.innerHTML = '';
+        this.gameover = false;
+        this.fields_of_game = ['', '', '', '', '', '', '', '', '']
+    },
+
+    draw() {
+        let field = '';
+        
+        for ( i in this.fields_of_game ) {
+            field += `<div onclick="jogo_da_velha.make_play(${i})">${this.fields_of_game[i]}</div>`
+        }
+        console.log(this.areaDoGame)
+        this.areaDoGame.innerHTML = field
+    }
 }
 
-function camposJogaveis() {
-    let camposDeJogo = document.querySelectorAll('.area-game div:not(.indisponivel)')
-    camposDeJogo.forEach(c => c.addEventListener('click', fazAJogadaNoClique))
-
-    let camposIndisponiveis = document.querySelectorAll('.area-game div.indisponivel')
-    camposIndisponiveis.forEach(c => c.removeEventListener('click', fazAJogadaNoClique))
-}
-verificarSeGanhou();
-camposJogaveis();
+buttonPlay.addEventListener('click', () => jogo_da_velha.init('.area-game'))
+buttonReset.addEventListener('click', () => jogo_da_velha.reset_game())
